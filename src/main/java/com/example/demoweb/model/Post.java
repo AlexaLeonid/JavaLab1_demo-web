501bdab7c1b017ae710bd886a2a4529b0151fd18
Leonidova Alexandra <alexandraleonid2002@gmail.ru>
2023-02-14 22:22:15 +0500

    Bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,11 +1,19 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private String likes = "0";
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate(){
+        return creationDate;
     }
 
     public void setText(String text){
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,13 +4,14 @@ import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 
 @Service
 public class PostService {
 
-    private List<Post> Posts = Arrays.asList(new Post("postik"), new Post("postttt"),new Post("poooost"));
+    private List<Post> Posts = Arrays.asList(new Post("Я не плачу, это просто слезы", new Date()), new Post("Человек-спичка плюс человек-электричка", new Date()),new Post("Умные мысли преследовали его, но он был быстрее", new Date()));
 
     public List<Post> listAllPosts(){
         return Posts;
