10a51403df4a7c0bb3fe11120f4aaf5cba03756e
Leonidova Alexandra <alexandraleonid2002@gmail.ru>
2023-02-14 23:36:50 +0500

    Create new post

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -10,11 +11,17 @@ import java.util.List;
 
 @Service
 public class PostService {
-
-    private List<Post> Posts = Arrays.asList(new Post("Я не плачу, это просто слезы", new Date()), new Post("Человек-спичка плюс человек-электричка", new Date()),new Post("Умные мысли преследовали его, но он был быстрее", new Date()));
+    private List<Post> posts;
+    PostService(){
+        posts = new ArrayList<>();
+        posts.addAll(Arrays.asList(new Post("Я не плачу, это просто слезы", new Date()), new Post("Человек-спичка плюс человек-электричка", new Date()),new Post("Умные мысли преследовали его, но он был быстрее", new Date())));
+    }
 
     public List<Post> listAllPosts(){
-        return Posts;
+        return posts;
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 
 }
